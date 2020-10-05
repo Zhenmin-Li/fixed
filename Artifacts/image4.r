2ecebb854ed7feb8a948af6e6e233e0787fc7f41
Sam Hawke <shawke@unc.edu>
2020-10-05 17:31:42 +0000

    updating image 4

--- Artifacts/image4.r
+++ Artifacts/image4.r
@@ -2,14 +2,19 @@ library(tidyverse)
 
 g <- tv %>% 
   filter(!is.na(duration)) %>% 
+  filter(release_year > 1980) %>% 
+  filter(rating %in% c("TV-PG", "TV-14", "TV-MA", "TV-G")) %>% 
   ggplot(data = .) +
-  geom_point(aes(x = release_year, y = num_seasons),
-             color = "purple") +
+  geom_point(aes(x = release_year, y = num_seasons, 
+                 color = rating)) +
+  geom_smooth(aes(x = release_year, y = num_seasons), se = F) +
+  geom_vline(aes(xintercept = 2005)) +
   labs(title = "Scatterplot of Number of Seasons of \
   Netflix TV Shows By Release Year",
        x = "Release Year",
-       y = "Number of Seasons")
+       y = "Number of Seasons") +
+  facet_wrap(~rating)
 
-aspect_ratio <- 2
+aspect_ratio <- 1.8
 height <- 7
 ggsave("Artifacts/image4.png", g, height = 7 , width = 7 * aspect_ratio)
