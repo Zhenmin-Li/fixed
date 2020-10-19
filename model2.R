3405b9a2808a8f538c8a4930e87bbb09ea3e4706
Sam Hawke <shawke@unc.edu>
2020-10-19 19:39:41 +0000

    edited Rmd file, added PDF of Report in case of issues

--- model2.R
+++ model2.R
@@ -5,14 +5,13 @@ tv <- read.csv("derived_data/tv.csv")
 m2 <- lm(num_seasons ~ release_year, data = tv)
 saveRDS(m2, "derived_data/linear_model2.rds")
 
-b <- coef(m2)[1]
-m <- coef(m2)[2]
 tv$residuals <- m2$residuals
 
 p1 <- ggplot(data = tv, aes(x = release_year, y = num_seasons)) +
   labs(title = "Number of Seasons by Release Year",
        x = "Release Year", y = "Number of Seasons") +
-  geom_point(color = "purple")
+  geom_point(color = "purple") +
+  geom_smooth(method = "lm", se = F)
 
 ggsave("derived_data/model2_plot1.png", p1)
 
